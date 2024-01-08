d6b92013e20e94618602cf48ba491c1d6273a874
Kanwalmeet Singh <kanwalmeet.singhkochar.ext@bayer.com>
2024-01-08 16:36:39 +0100

    updates for COLID2.18

--- src/COLID.Scheduler.Web/Startup.cs
+++ src/COLID.Scheduler.Web/Startup.cs
@@ -1,67 +1,67 @@
-﻿using COLID.Identity;
-using COLID.Scheduler.ExceptionMiddleware;
-using COLID.Scheduler.Services;
-using COLID.SchedulerService.Hangfire;
-using CorrelationId;
-using CorrelationId.DependencyInjection;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using COLID.StatisticsLog;
-using COLID.Common.Logger;
+﻿//using COLID.Identity;
+//using COLID.Scheduler.ExceptionMiddleware;
+//using COLID.Scheduler.Services;
+//using COLID.SchedulerService.Hangfire;
+//using CorrelationId;
+//using CorrelationId.DependencyInjection;
+//using Microsoft.AspNetCore.Builder;
+//using Microsoft.AspNetCore.Hosting;
+//using Microsoft.AspNetCore.Http;
+//using Microsoft.Extensions.Configuration;
+//using Microsoft.Extensions.DependencyInjection;
+//using Microsoft.Extensions.Hosting;
+//using Microsoft.Extensions.Logging;
+//using COLID.StatisticsLog;
+//using COLID.Common.Logger;
 
-namespace COLID.SchedulerService
-{
-    public class Startup
-    {
-        public IConfiguration Configuration { get; }
+//namespace COLID.SchedulerService
+//{
+//    public class Startup
+//    {
+//        public IConfiguration Configuration { get; }
 
-        public IHostEnvironment Environment { get; }
+//        public IHostEnvironment Environment { get; }
 
-        public Startup(IConfiguration configuration, IWebHostEnvironment env)
-        {
-            Configuration = new ConfigurationBuilder().AddConfiguration(configuration).Build();
-            Environment = env;
-        }
+//        public Startup(IConfiguration configuration, IWebHostEnvironment env)
+//        {
+//            Configuration = new ConfigurationBuilder().AddConfiguration(configuration).Build();
+//            Environment = env;
+//        }
 
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddHttpContextAccessor();
-            services.AddTransient<IHttpContextAccessor, HttpContextAccessor>();
-            services.AddHttpClient();
-            services.AddHealthChecks();
-            services.AddDefaultCorrelationId();
-            services.AddCorrelationIdLogger();
+//        public void ConfigureServices(IServiceCollection services)
+//        {
+//            services.AddHttpContextAccessor();
+//            services.AddTransient<IHttpContextAccessor, HttpContextAccessor>();
+//            services.AddHttpClient();
+//            services.AddHealthChecks();
+//            services.AddDefaultCorrelationId();
+//            services.AddCorrelationIdLogger();
 
-            services.AddLogging();
-            services.AddServicesModule(Configuration);
+//            services.AddLogging();
+//            services.AddServicesModule(Configuration);
 
-            services.RegisterJobs();
-            services.RegisterHangfire(Configuration);
+//            services.RegisterJobs();
+//            services.RegisterHangfire(Configuration);
 
-            services.AddIdentityModule(Configuration);
+//            services.AddIdentityModule(Configuration);
 
-            services.AddStatisticsLogModule(Configuration);
-        }
+//            services.AddStatisticsLogModule(Configuration);
+//        }
 
-        public void Configure(IApplicationBuilder app)
-        {
-            app.UseCorrelationId();
-            app.UseRouting();
+//        public void Configure(IApplicationBuilder app)
+//        {
+//            app.UseCorrelationId();
+//            app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapHealthChecks("/health");
-            });
+//            app.UseEndpoints(endpoints =>
+//            {
+//                endpoints.MapHealthChecks("/health");
+//            });
 
-            app.SetupHangfireServer(Configuration);
-            app.SetupHangfireJobs(Configuration);
+//            app.SetupHangfireServer(Configuration);
+//            app.SetupHangfireJobs(Configuration);
 
-            app.UseExceptionMiddleware();
-        }
-    }
-}
+//            app.UseExceptionMiddleware();
+//        }
+//    }
+//}
